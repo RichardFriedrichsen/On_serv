756dd6787e409b3f879e3f6d00d203bc0a0202a7
Richard <richard_friedrichsen@yahoo.de>
2023-06-21 02:12:50 +0200

    Login page styling

--- On_Serv/main/views.py
+++ On_Serv/main/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from .models import Vehicle, Company, Service
 from .forms import add_vehicle_form, add_company_form, add_service_form
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 # About Us page
+@login_required()
 def about(request):
     return render(request, "about.html")
 
-
+@login_required()
 def list_vehicles(request):
     vehicle_list = Vehicle.objects.all()
     return render(request, "vehicles.html", {'vehicle_list': vehicle_list })
 
-
+@login_required()
 def add_vehicle(request):
     submitted = False
 
@@ -30,10 +32,12 @@ def add_vehicle(request):
 
     return render(request, "add_vehicle.html", {"form": form, 'submitted': submitted})
 
+@login_required()
 def list_companies(request):
     company_list = Company.objects.all()
     return render(request, "companies.html",{"company_list": company_list} )
 
+@login_required()
 def add_company(request):
     submitted = False
 
@@ -50,10 +54,12 @@ def add_company(request):
 
     return render(request, "add_company.html", {"form": form, 'submitted': submitted})
 
+@login_required()
 def list_services(request):
     service_list = Service.objects.all()
     return render(request, "services.html", {"service_list": service_list})
 
+@login_required()
 def add_service(request):
     submitted = False
 
